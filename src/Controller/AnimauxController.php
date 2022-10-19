7c394e87a1144da68d1a29aee42c115c289d053f
CaliDavin <davin.calimouttoupoulle@euro-isolation.com>
2022-10-19 17:53:49 +0200

    ajout/update/modif

--- src/Controller/AnimauxController.php
+++ src/Controller/AnimauxController.php
@@ -2,11 +2,17 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\Animal;
+use App\Form\AnimalType;
+use App\Service\UploadFile;
+use App\Repository\AnimalRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class AnimauxController extends AbstractController
 {
@@ -36,4 +42,122 @@ class AnimauxController extends AbstractController
         ]);
     }
 
+
+    #[Route('/animaux/add', name:'add_animaux', methods:["GET", "POST"])]
+    public function add(Request $request): Response
+    {
+        $animaux = new Animal;
+        // $this->createFormBuilder() permet de créer un générateur de formulaire
+        // $form = $this->createFormBuilder($category)
+        //    // On utilise la méthode add pour ajouter des champs à notre formulaire
+        //     ->add('name', TextType::class, [
+        //         'label' => "Nom de la catégorie",
+        //         'attr' => [
+        //             'placeholder' => "Nom de la catégorie"
+        //         ],
+        //         'row_attr' => [
+        //             'class' => 'mb-3 form-floating'
+        //         ]
+        //     ])
+        //     ->add('button', SubmitType::class, [
+        //         'label' => "Ajouter"
+        //     ])
+        //     // La méthode getForm permet de récupérer le formulaire généré
+        //     ->getForm()
+        // ;
+        $form = $this->createForm(AnimalType::class, $animaux);
+        // handleRequest() récupère les informations reçues du formulaire
+        // et stockée dans la Request pour les associer à l'entité Category
+        // passée en data du createFormBuilder
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $picture = $form->get('photo')->getData();
+            if ($picture) {
+                try {
+                    $pictureName = md5(uniqid()). '.' . $picture->guessExtension();
+                    // Permet de déplacer l'image dans un dossier d'upload
+                    // move() prend 2 paramètres: le dossier d'upload et le nom du fichier
+                    $picture->move(
+                        $this->getParameter('upload_dir'),
+                        $pictureName
+                    );
+                    $animaux->setPhoto($pictureName);
+                } catch (FileException $e) {
+                    $this->addFlash('danger', "Une erreur s'est produite lors de l'enregistrement de l'image avec le message suivant: ". $e->getMessage());
+                }
+            }
+            $animaux->setCreatedAt(new DateTime());
+            $om = $this->manager->getManager();
+            $om->persist($animaux);
+            $om->flush();
+            $this->addFlash('success', "L'animal ".$animaux->getName()." a été ajoutée");
+
+            return $this->redirectToRoute('app_animaux');
+        }
+
+        // Lorsqu'on passe un formulaire à notre template,
+        // On utilise la méthode renderForm plutôt que render
+        // qui va appliquer la méthdoe createView() sur le form 
+        // pour en générer l'affichage
+        return $this->renderForm('animaux/add.html.twig', [
+            // 'form' => $form->createView()
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/{id}/update', name:'update', methods:['GET', 'POST'], requirements:['id' => "\d+"])]
+    public function update(int $id, Request $request, AnimalRepository $animalRepository): Response
+    {
+        $animaux = $animalRepository->find($id);
+        if (!$animaux) {
+            $this->addFlash('danger', "L'animal que vous recherchez n'existe pas.");
+            return $this->redirectToRoute('app_animaux');
+        }
+
+        $form = $this->createForm(AnimalType::class, $animaux);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $picture = $form->get('photo')->getData();
+            if ($picture) {
+                try {
+                    if ($animaux->getPhoto()) {
+                        unlink($this->getParameter('upload_dir') .'/' . $animaux->getPhoto());
+                    }
+                    $pictureName = md5(uniqid()). '.' . $picture->guessExtension();
+                    // Permet de déplacer l'image dans un dossier d'upload
+                    // move() prend 2 paramètres: le dossier d'upload et le nom du fichier
+                    $picture->move(
+                        $this->getParameter('upload_dir'),
+                        $pictureName
+                    );
+                    $animaux->setPhoto($pictureName);
+                } catch (FileException $e) {
+                    $this->addFlash('danger', "Une erreur s'est produite lors de l'enregistrement de l'image avec le message suivant: ". $e->getMessage());
+                }
+            }
+
+            $animalRepository->save($animaux, true);
+            $this->addFlash('success', "L'article a été modifié.");
+            return $this->redirectToRoute('app_animaux');
+        }
+
+        return $this->renderForm("animaux/update.html.twig", [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/{id}/delete', name:"delete", requirements:['id' => "\d+"])]
+    public function delete(int $id, AnimalRepository $animalRepository): Response
+    {
+        $animaux = $animalRepository->find($id);
+        if ($animaux) {
+            $animalRepository->remove($animaux, true);
+        } else {
+            $this->addFlash('danger', "L'animal que vous recherchez n'existe pas.");
+        }
+        return $this->redirectToRoute('app_animaux');
+    }
 }
