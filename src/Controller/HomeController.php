a62e5e35e47a7e6e4802cc0ac9b86b18c1d13f3b
CaliDavin <davin.calimouttoupoulle@euro-isolation.com>
2022-10-20 17:55:53 +0200

    page d'accueil presque fini

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,56 +1,33 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Animal;
+use App\Repository\AnimalRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController{
 
-/**
- * Une méthode de controller doit obligatoirement retourner une Response pour afficher une page
- *
- * @return Response
- */
-    public function hello(): Response
-    {
-        return new Response('<h1>Hello!</h1>');
-    }
-
-    /**
-     * Documentation d'une méthode à partir de symfony 4
-     * La route et d'autres éléments sont ajoutés au bloc de documentation
-     * et le système d'annotation de PHP est utilisé pour charger le composant Routing
-     * 
-     * Les attributs sont arrivés avec PHP8 et on permis de remplacer les annotations.
-     * On peut ainsi se servir des extension pour charger l'attribut en question 
-     * et voir la documentation des paramètres utilisables.
-     *
-     * @return Response
-     * @ Route(path="/bye", name="bye")
-     */
-    #[Route(path:"/bye", name:"bye", methods:["GET"])]
-    public function bye(): Response
-    {
-        return new Response('<h1>Au revoir </h1>');
-    }
+    public function __construct(
+        private ManagerRegistry $manager
+    ){}
 
     #[Route("/", name:"app_home", methods:["GET"])]
-    public function home(): Response
+    public function home(AnimalRepository $animalRepository): Response
     {
-        $categories = [
-            [
-                'name' => "Catégorie 1"
-            ],
-            [
-                'name' => "Catégorie 2"
-            ],
-            [
-                'name' => "Catégorie 3"
-            ],
-        ];
-        return $this->render("home.html.twig", [
-            'categories' => $categories
+        $date = date('Y-m-d h:i:s', strtotime("-30 days"));
+        $animaux = $animalRepository->createQueryBuilder('a')
+                ->where('a.createdAt BETWEEN :n30days AND :today')
+                ->setParameter('today', date('Y-m-d h:i:s'))
+                ->setParameter('n30days', $date)
+                ->getQuery()
+                ->getResult();
+            ;
+
+        return $this->render('home.html.twig', [
+            'animaux' => $animaux,
         ]);
     }
 }
